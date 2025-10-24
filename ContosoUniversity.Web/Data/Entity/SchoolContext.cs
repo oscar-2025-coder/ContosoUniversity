20816bd4bcd8fb2a4e83742668cbdd9e37970c42
oscar leonardo ochoaperez <oscarleonardoochoaperez@gmail.com>
2025-10-24 01:08:55 -0500

    Avance en Entity Framework

--- ContosoUniversity.Web/Data/Entity/SchoolContext.cs
+++ ContosoUniversity.Web/Data/Entity/SchoolContext.cs
@@ -10,5 +10,12 @@ public class SchoolContext : DbContext
     }
     public DbSet<Course> Courses { get; set; }
     public DbSet<Enrollment> Enrollments { get; set; }
+    public DbSet<Student> Students { get; set; }
     
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>().ToTable("Course");
+        modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+        modelBuilder.Entity<Student>().ToTable("Student");
+    }
 }
--- ContosoUniversity.Web/Models/Course.cs
+++ ContosoUniversity.Web/Models/Course.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations.Schema;
 namespace ContosoUniversity.Web.Models;
 
 public class Course
